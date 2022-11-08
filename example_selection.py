d08f51d11725daf2c0dc9988f9678eb6078badd8
KwixjSan <afiqfirdaus246@gmail.com>
2022-11-08 14:25:09 +0800

    week4example

---
+++ example_selection.py
@@ -0,0 +1,9 @@
+first_int=input(" Enter input First int:")
+first_int=float(first_int)
+second_int=input(" Enter input second int:")
+second_int=float(second_int)
+print("The biggest number is:")
+if first_int > second_int:
+	print ("First")
+else:
+	print("Second")
