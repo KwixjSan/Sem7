01f2f5d187c4d4b9bb4473f1010db808df2b5c40
KwixjSan <afiqfirdaus246@gmail.com>
2022-12-13 15:19:11 +0800

    afiqcommit

---
+++ test1/test.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+numbers = [ 1,2,3,4,5,6,7,8,9,8,8,9,9]
+
+
+my_array_1 = np.arange (6)
+my_array_2 =np.arange (5,10)
+my_array_3 =np.arange (10,1,-3) #(max,min,space)
+
+my_float_array=np.linspace (0.0,1.0, num=10)
+
+print(my_array_1)
+print(my_array_2)
+print(my_array_3)
+print(my_float_array)
---
+++ test1/test2.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+grades = np.array ([[87,96,70],[100,87,90],[100,81,82]])
+
+grades_sum= grades.sum()
+grades_min= grades.min()
+grades_max= grades.max()
+grades_sum= grades.sum()
+grades_sum= grades.sum()
